6dc9e2bb3755360602cff2c7498b0b646b12de3a
Leonardo-Pico <63673074+Leonardo-Pico@users.noreply.github.com>
2022-06-28 16:33:48 -0500

    Update Video

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -11,7 +11,7 @@
     p.my-5(data-aos="fade-up-right") La transformación digital ha irrumpido en cada rincón del planeta y se hizo más notoria, dando continuidad a los nuevos modos de vida que se volvieron el cotidiano de todos con la aparición de la pandemia en el año 2020. Lo invitamos a ver el siguiente video que lo introducirá a la temática a abordar:
     figure.mb-5(data-aos="fade-up-left")
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/D-rgLJyi5ss" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/KjRdWt1M70Y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
     
 </template>
 
